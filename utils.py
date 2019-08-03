437de99613302a3c2306e8c38bc59699d260d3b3
ishanrai05 <ishanrai05@gmail.com>
2019-08-03 15:21:12 +0530

    Adds utility tools

---
+++ utils.py
@@ -0,0 +1,70 @@
+import torch
+from torchvision.utils import Dataset
+import numpy as np
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+        # Load data and get label
+        X = Image.open(self.df['path'][index])
+        y = torch.tensor(int(self.df['cell_type_idx'][index]))
+
+        if self.transform:
+            X = self.transform(X)
+        return X, y
+
+# this function is used during training process, to calculation the loss and accuracy
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def compute_img_mean_std(image_paths):
+    """
+        computing the mean and std of three channel on the whole dataset,
+        first we should normalize the image from 0-255 to 0-1
+    """
+
+    img_h, img_w = 224, 224
+    imgs = []
+    means, stdevs = [], []
+
+    for i in tqdm(range(len(image_paths))):
+        img = cv2.imread(image_paths[i])
+        img = cv2.resize(img, (img_h, img_w))
+        imgs.append(img)
+
+    imgs = np.stack(imgs, axis=3)
+    print(imgs.shape)
+
+    imgs = imgs.astype(np.float32) / 255.
+
+    for i in range(3):
+        pixels = imgs[:, :, i, :].ravel()  # resize to one row
+        means.append(np.mean(pixels))
+        stdevs.append(np.std(pixels))
+
+    means.reverse()  # BGR --> RGB
+    stdevs.reverse()
+
+    print("normMean = {}".format(means))
+    print("normStd = {}".format(stdevs))
+    return means,stdevs
