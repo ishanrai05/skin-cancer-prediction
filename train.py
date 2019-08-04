39219cc6042646116ad3e4baa7ee5edf75bdb658
ishanrai05 <ishanrai05@gmail.com>
2019-08-04 12:34:31 +0530

    Adds train method

---
+++ train.py
@@ -0,0 +1,39 @@
+import torch
+from torch.autograd import Variable
+
+from utils import AverageMeter
+
+def train(train_loader, model, criterion, optimizer, epoch, device):
+    model.train()
+
+    train_loss = AverageMeter()
+    train_acc = AverageMeter()
+
+    total_loss_train, total_acc_train = [],[]
+
+    curr_iter = (epoch - 1) * len(train_loader)
+
+    for i, data in enumerate(train_loader):
+        images, labels = data
+        N = images.size(0)
+        # print('image shape:',images.shape, 'label shape',labels.shape)
+        images = Variable(images).to(device)
+        labels = Variable(labels).to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        prediction = outputs.max(1, keepdim=True)[1]
+        train_acc.update(prediction.eq(labels.view_as(prediction)).sum().item()/N)
+        train_loss.update(loss.item())
+        curr_iter += 1
+        if (i + 1) % 100 == 0:
+            print('[epoch %d], [iter %d / %d], [train loss %.5f], [train acc %.5f]' % (
+                epoch, i + 1, len(train_loader), train_loss.avg, train_acc.avg))
+            total_loss_train.append(train_loss.avg)
+            total_acc_train.append(train_acc.avg)
+    return train_loss.avg, train_acc.avg, total_loss_train, total_acc_train
+
