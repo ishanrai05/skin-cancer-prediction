e8e0df94bb9937ce6bee16900d3e972331c12b00
ishanrai05 <ishanrai05@gmail.com>
2019-08-05 01:12:43 +0530

    Adds validata method

--- train.py
+++ train.py
@@ -37,3 +37,26 @@ def train(train_loader, model, criterion, optimizer, epoch, device):
             total_acc_train.append(train_acc.avg)
     return train_loss.avg, train_acc.avg, total_loss_train, total_acc_train
 
+def validate(val_loader, model, criterion, optimizer, epoch, device):
+    model.eval()
+    val_loss = AverageMeter()
+    val_acc = AverageMeter()
+    
+    with torch.no_grad():
+        for _, data in enumerate(val_loader):
+            images, labels = data
+            N = images.size(0)
+            images = Variable(images).to(device)
+            labels = Variable(labels).to(device)
+
+            outputs = model(images)
+            prediction = outputs.max(1, keepdim=True)[1]
+
+            val_acc.update(prediction.eq(labels.view_as(prediction)).sum().item()/N)
+
+            val_loss.update(criterion(outputs, labels).item())
+
+    print('------------------------------------------------------------')
+    print('[epoch %d], [val loss %.5f], [val acc %.5f]' % (epoch, val_loss.avg, val_acc.avg))
+    print('------------------------------------------------------------')
+    return val_loss.avg, val_acc.avg
